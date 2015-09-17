f3a0264f83e4305826361769786a5d5ef4a9ffb2
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-09-16 22:46:32 -0500

    Console command class added. It will make a user's life simple for adding a new language to the database.

--- src/Translatable/Console/Commands/AddDatabaseLanguage.php
+++ src/Translatable/Console/Commands/AddDatabaseLanguage.php
@@ -1,15 +1,69 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rulrok
- * Date: 16/09/15
- * Time: 10:10 PM
- */
 
-namespace Translatable\Console\Commands;
+namespace panopla\Translatable\Console\Commands;
 
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 
-class AddDatabaseLanguage
+class AddDatabaseLanguage extends Command
 {
 
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'translatable:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new language to the database.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        //
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['code', null, InputOption::VALUE_REQUIRED, 'An example option.', null],
+            ['name', null, InputOption::VALUE_REQUIRED, 'An example option.', null],
+        ];
+    }
 }
